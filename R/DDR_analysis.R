0102b80097789da90b66226a092f8428ac3531ce
Lingshu Hu <lingshu.hu@hotmail.com>
2020-10-26 01:53:04 -0500

    mlm

--- R/DDR_analysis.R
+++ R/DDR_analysis.R
@@ -53,8 +53,27 @@ print(mod.f$loadings,sort=T,cut=.3)
 cor(ntDR[, 1:10])
 
 ### MLM
+nt$id <- 1:nrow(nt)
+nt_long <- tidyr::gather(nt, key = "moral_dim", value = "loading", 3:12)
 
-mlm <- lme4::lmer()
+nt_long$moral_dim <- factor(nt_long$moral_dim)
+nt_long$moral_dim <- relevel(nt_long$moral_dim, ref = "fairness")
+mlm <- lme4::lmer(loading ~ party + moral_dim + #party * moral_dim + 
+                    (1 | year) + (1 | year:round), nt_long)
+summary(mlm)
+sjstats::icc(mlm)
+performance::icc(mlm)
+# get icc
+# https://stats.stackexchange.com/questions/174071/how-to-compute-intraclass-correlation-icc-for-three-level-negative-binomial-hi
+
+alpha <- lme4::getME(mlm, "sigma")^2 # invividual variance
+
+vars <- VarCorr(mlm)
+var_l2 <- vars[[1]][1]
+var_l3 <- vars[[2]][1]
+
+ICC_l2 <- (var_l3 + var_l2)/(var_l3 + var_l2 + alpha) 
+ICC_l3 <- var_l3/(var_l3 + var_l2 + alpha) 
 
 
 
